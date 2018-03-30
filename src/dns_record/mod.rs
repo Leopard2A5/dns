494d127bcaa91de04030e3cb67b74ba25ff7e585
René Perschon <rperschon85@gmail.com>
2018-03-30 21:31:31 +0200

    added opcode function

--- src/dns_record/mod.rs
+++ src/dns_record/mod.rs
@@ -1,3 +1,29 @@
+use std::result;
+use std::borrow::Cow;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum DnsMsgError {
+    InvalidData,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Error<'a> {
+    pub kind: DnsMsgError,
+    pub msg:Cow<'a, str>
+}
+
+impl<'a> Error<'a> {
+    pub fn new<M>(kind: DnsMsgError, msg: M) -> Self
+    where M: Into<Cow<'a, str>> {
+        Self {
+            kind,
+            msg: msg.into()
+        }
+    }
+}
+
+pub type Result<'a, T> = result::Result<T, Error<'a>>;
+
 pub struct DnsRecord {
     data: [u8; 512]
 }
@@ -21,12 +47,28 @@ impl DnsRecord {
             QR::RESPONSE
         }
     }
+
+    pub fn opcode(&self) -> Result<OPCODE> {
+        match self.data[2] >> 3 & 0x0f {
+            0 => Ok(OPCODE::QUERY),
+            1 => Ok(OPCODE::IQUERY),
+            2 => Ok(OPCODE::STATUS),
+            x => Err(Error::new(DnsMsgError::InvalidData, format!("Unknown opcode {}", x)))
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum QR {
     QUERY,
     RESPONSE
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum OPCODE {
+    QUERY,
+    IQUERY,
+    STATUS
 }
 
 #[cfg(test)]
@@ -57,5 +99,35 @@ mod test {
         buffer[2] = 0xa0;
         let rec = DnsRecord::new(buffer);
         assert_eq!(QR::RESPONSE, rec.qr());
+    }
+
+    #[test]
+    fn should_read_opcodes() {
+        let mut buffer = [0u8; 512];
+
+        // 1 are the relevant bits for opcode in the third byte
+        // 0111 1000
+        // 0000 0000 = 0x00 0: query
+        // 0000 1000 = 0x08 1: iquery
+        // 0001 0000 = 0x10 2: status
+
+        buffer[2] = 1;
+        let rec = DnsRecord::new(buffer);
+        assert_eq!(Ok(OPCODE::QUERY), rec.opcode());
+
+        buffer[2] = 1 << 3;
+        let rec = DnsRecord::new(buffer);
+        assert_eq!(Ok(OPCODE::IQUERY), rec.opcode());
+
+        buffer[2] = 2 << 3;
+        let rec = DnsRecord::new(buffer);
+        assert_eq!(Ok(OPCODE::STATUS), rec.opcode());
+
+        for i in 3..16 {
+            buffer[2] = i << 3;
+            let rec = DnsRecord::new(buffer);
+            let msg = format!("Unknown opcode {}", i);
+            assert_eq!(Err(Error::new(DnsMsgError::InvalidData, msg)), rec.opcode());
+        }
     }
 }
