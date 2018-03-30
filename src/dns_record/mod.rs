959c17330cb1b028ec83da3b9336f8331739cbc2
René Perschon <rperschon85@gmail.com>
2018-03-30 20:32:57 +0200

    added qr function

--- src/dns_record/mod.rs
+++ src/dns_record/mod.rs
@@ -7,12 +7,26 @@ impl DnsRecord {
         DnsRecord { data }
     }
 
-    fn id(&self) -> u16 {
+    pub fn id(&self) -> u16 {
         let tmp = &self.data[0] as *const u8;
         unsafe {
             *(tmp as *const u16) 
         }
     }
+
+    pub fn qr(&self) -> QR {
+        if self.data[2] & 0xa0 == 0 {
+            QR::QUERY
+        } else {
+            QR::RESPONSE
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum QR {
+    QUERY,
+    RESPONSE
 }
 
 #[cfg(test)]
@@ -29,5 +43,19 @@ mod test {
         buffer[0] = 1;
         let rec = DnsRecord::new(buffer);
         assert_eq!(0x0101u16, rec.id());
+    }
+
+    #[test]
+    fn should_read_qr() {
+        let mut buffer = [0u8; 512];
+        buffer[0] = 0xff;
+        buffer[1] = 0xff;
+
+        let rec = DnsRecord::new(buffer);
+        assert_eq!(QR::QUERY, rec.qr());
+
+        buffer[2] = 0xa0;
+        let rec = DnsRecord::new(buffer);
+        assert_eq!(QR::RESPONSE, rec.qr());
     }
 }
